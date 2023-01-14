97656f32b494b6ef6492588ea35857d42c7e7f7e
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-01-14 12:43:09 +0900

    htmlタグのlang属性をjaに変更し、descriptionを記述し、URLをlocalhost:3000に設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		host: true,
+		port: 3000,
 	}
 };
 
