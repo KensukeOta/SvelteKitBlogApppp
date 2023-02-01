90369241bab143fab63184f0d1407ffefa84539e
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-02-01 18:37:18 +0900

    ファイルのアップロードに必要なコンポーネントを実装

---
+++ src/lib/components/atoms/InputFile.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let accept: string;
+  export let name: string;
+  export let id: string;
+  export let className: string;
+</script>
+
+<input type="file" {accept} {name} {id} class={className} />
---
+++ src/lib/components/molecules/AvatarArea.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import InputFile from "../atoms/InputFile.svelte";
+
+  export let accept: string;
+  export let name: string;
+  export let id: string;
+  export let className: string;
+
+  const avatarProps = {
+    accept,
+    name,
+    id,
+    className,
+  };
+</script>
+
+<label for="avatar">Profile Image <span class="text-red-500">*Option</span></label>
+<InputFile {...avatarProps} />
